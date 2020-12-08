48e88096af82579709285e413afc9e3c220259da
tranbaohieu <tranbaohieu98@gmail.com>
2020-12-08 18:22:10 +0700

    add code

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,18 +3,29 @@ import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
 
 import GoogleLogin from 'react-google-login';
+import  { Redirect } from 'react-router-dom'
 
 
 const Login = () => {
   const responseFacebook = (response) => {
     localStorage.setItem('name', response.name);
     localStorage.setItem('avatar', response.picture.data.url)
+    window.location.reload(true);
   }
-
+  const redirectHome = () => {
+    window.location.reload(true);
+    return <Redirect to='/' />
+  }
   const responseGoogle = (response) => {
-    console.log(response);
+    console.log(response)
+    localStorage.setItem('name', response.Nw.profileObj.name);
+    localStorage.setItem('avatar', response.Nw.profileObj.imageUrl);
+    // window.location.reload(true);
   }
-
+  // const responseGoogle = (response) => {
+  //   console.log(response.Nw)
+  //   // window.location.reload(true);
+  // }
   return (
     <div className="App">
       <h1>Please login</h1>
@@ -27,10 +38,9 @@ const Login = () => {
     <br />
     <br />
     <GoogleLogin
-      clientId=""
+      clientId="116556632783-lcj18jn3usv1fp6mh77vvtjjapls8rhs.apps.googleusercontent.com"
       buttonText="LOGIN WITH GOOGLE"
       onSuccess={responseGoogle}
-      onFailure={responseGoogle}
     />
     </div>
   );
